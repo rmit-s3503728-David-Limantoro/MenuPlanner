cac4b6afa3afbe290f3105d9039ce0e7c4e22c1e
David Limantoro (s3503728) <s3503728@student.rmit.edu.com>
2017-09-19 16:02:12 +1000

    Added S3 for potential picture upload capability

--- api/controllers/RecipeController.js
+++ api/controllers/RecipeController.js
@@ -13,6 +13,11 @@ AWS.config.update({
   endpoint: "https://dynamodb.us-west-2.amazonaws.com"
 });
 var dynamodb = new AWS.DynamoDB();
+var s3 = new AWS.S3();
+s3.abortMultipartUpload(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
 
 module.exports = {
   _config: {
