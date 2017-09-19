777f6e9819fc6d8da267af4e5ef223c8e2b93348
David Limantoro (s3503728) <s3503728@student.rmit.edu.com>
2017-09-19 16:08:11 +1000

    Added docClient for querying dynamodb

--- api/controllers/RecipeController.js
+++ api/controllers/RecipeController.js
@@ -13,6 +13,7 @@ AWS.config.update({
   endpoint: "https://dynamodb.us-west-2.amazonaws.com"
 });
 var dynamodb = new AWS.DynamoDB();
+var docClient = new AWS.DynamoDB.DocumentClient();
 var s3 = new AWS.S3();
 s3.abortMultipartUpload(params, function (err, data) {
   if (err) console.log(err, err.stack); // an error occurred
