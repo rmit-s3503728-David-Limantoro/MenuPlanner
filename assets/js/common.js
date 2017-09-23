0aafdc1a97489989e50fba29e9f6d5188c4e6f9c
David Limantoro <s3503728@student.rmit.edu.au>
2017-09-23 14:57:47 +1000

    Implemented popup message notification
    Also added common.js which contains functions common for most pages in the website

---
+++ assets/js/common.js
@@ -0,0 +1,49 @@
+function ajaxRequest(method, url, data, doneCallback, failCallback) {
+    $.ajax({
+        url: url,
+        method: method,
+        data: data
+    }).done(doneCallback).fail(failCallback);
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+function popupAlert(message, alert_classtype, extraFunction_before, extraFunction_after) {
+  if (!(extraFunction_before === undefined || extraFunction_before === null)) {
+    extraFunction_before();
+  }
+  $('#floatAlertDiv').addClass(alert_classtype);
+  $('#floatAlertDiv').text(message);
+  $('#floatAlertDiv').slideDown("slow", function () {
+    setTimeout(function () {
+      $('#floatAlertDiv').slideUp("slow", function () {
+        $('#floatAlertDiv').removeClass(alert_classtype);
+        $('#floatAlertDiv').text("");
+        if (!(extraFunction_after === undefined || extraFunction_after === null)) {
+          extraFunction_after();
+        }
+      })
+    }, 2000);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (getUrlParameter('uploadSuccess') === "true") {
+        popupAlert("Recipe is uploaded successfully", "");
+    } else if (getUrlParameter('uploadSuccess') === "false") {
+        popupAlert("There is a problem uploading the recipe", "");
+    }
+}, false);
